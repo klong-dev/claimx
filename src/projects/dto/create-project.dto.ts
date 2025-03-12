badccd951c49d77254a3007dc3acde4fa1a811a5
Hoàng Kim Long <longhoang8204@gmail.com>
2025-03-12 10:01:38 +0700

    mod(Project): add status when create

--- src/projects/dto/create-project.dto.ts
+++ src/projects/dto/create-project.dto.ts
@@ -1,4 +1,4 @@
-import { IsString, IsNotEmpty } from 'class-validator';
+import { IsString, IsNotEmpty, IsNumber } from 'class-validator';
 
 export class CreateProjectDto {
     @IsString()
@@ -12,4 +12,7 @@ export class CreateProjectDto {
     @IsString()
     @IsNotEmpty()
     endDate: Date;
+
+    @IsNumber()
+    status: number;
 }
