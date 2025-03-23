a4c7c0c2a056016bd8f12e8d998dced2baa9a227
Hoàng Kim Long <longhoang8204@gmail.com>
2025-03-23 13:37:03 +0700

    fix: missing jwt service

--- src/comments/comments.module.ts
+++ src/comments/comments.module.ts
@@ -4,10 +4,20 @@ import { CommentsController } from './comments.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Comment } from './entities/comment.entity';
 import { User } from 'src/users/entities/user.entity';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([Comment, User]),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: { expiresIn: configService.get<string>('JWT_EXPIRES_IN') },
+      }),
+    }),
   ],
   controllers: [CommentsController],
   providers: [CommentsService],
