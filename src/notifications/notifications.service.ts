b73c9172b89a27ad865614f2473b820089af567e
Hoàng Kim Long <longhoang8204@gmail.com>
2025-03-31 00:09:54 +0700

    mod: return value of create noti

--- src/notifications/notifications.service.ts
+++ src/notifications/notifications.service.ts
@@ -34,7 +34,7 @@ export class NotificationsService {
             html: `<p>${message}</p>`,
         });
 
-        return notification;
+        return new HttpException('Notification sent successfully', 200);
     }
 
     async getNotifications(userId): Promise<Notification[]> {
